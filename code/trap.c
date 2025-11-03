5dfdd0e6cc4ee4420204a94d001fb064915e7a64
Q-upward <3306943533@qq.com>
2025-11-03 14:00:33 +0800

    Update trap.c

--- code/trap.c
+++ code/trap.c
@@ -196,20 +196,27 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB3 CHALLENGE3   YOUR CODE :  */
+             /* LAB3 CHALLENGE3   YOUR CODE :2310421  */
              /*(1)输出指令异常类型（ Illegal instruction）
               *(2)输出异常指令地址
               *(3)更新 tf->epc寄存器
               */
-             break;
+            cprintf("Exception type: Illegal instruction\n");
+            cprintf("Illegal instruction caught at 0x%lx\n", tf->epc);
+            tf->epc += 4;
+            break;
+
         case CAUSE_BREAKPOINT:
              //断点异常处理
-             /* LAB3 CHALLLENGE3   YOUR CODE :  */
+             /* LAB3 CHALLLENGE3   YOUR CODE : 2312103 */
              /*(1)输出指令异常类型（ breakpoint）
               *(2)输出异常指令地址
               *(3)更新 tf->epc寄存器
               */
-             break;
+            cprintf("Exception type: breakpoint\n");
+            cprintf("ebreak caught at 0x%lx\n", tf->epc);
+            tf->epc += 4;
+            break;
         case CAUSE_MISALIGNED_LOAD:
             break;
         case CAUSE_FAULT_LOAD:
@@ -270,3 +277,4 @@ void trap(struct trapframe *tf) {
     // dispatch based on what type of trap occurred
     trap_dispatch(tf);
 }
+
